0c32539cc6961e6e4180fed0e47500dfc0b0eefa
刘少杰 <439655668@qq.com>
2018-02-28 20:41:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/myappdatabase')
+
+var User = require('./user');
+
+// create a new user called chris
+var chris = new User({
+  name: 'Chris',
+  username: 'sevilayha',
+  password: 'password' 
+});
+
+// call the custom method. this will just add -dude to his name
+// user will now be Chris-dude
+chris.dudify(function(err, name) {
+  if (err) throw err;
+
+  console.log('Your new name is ' + name);
+});
+
+// call the built-in save method to save to the database
+chris.save(function(err) {
+  if (err) throw err;
+
+  console.log('User saved successfully!');
+});
+
+// get all the users
+User.find({}, function(err, users) {
+  if (err) throw err;
+
+  // object of all the users
+  console.log(users);
+});
